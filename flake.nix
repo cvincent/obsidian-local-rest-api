dd10732a0129f68b0bcdc216123ff73c5d85e9d0
Chris Vincent <chris.vincent@hey.com>
2025-02-20 21:20:01 -0600

    Update manifest

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Flake for working on obsidian-local-rest-api";
+
+  # Find package versions at nixhub.io
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pkgs.nodejs
+        ];
+
+        shellHook = ''
+          echo "Node $(node --version)"
+          echo ""
+          echo "LFG."
+        '';
+      };
+    };
+}
